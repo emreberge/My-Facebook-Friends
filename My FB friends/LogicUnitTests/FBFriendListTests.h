ff05eaf5378bd4102b9ecbe2b2553b30e97dbebf
Emre Berge Ergenekon <emre.b.ergenekon@gmail.com>
2012-02-08 02:31:51 -0800

    Created a test class for the upcoming FBFriendList

---
+++ My FB friends/LogicUnitTests/FBFriendListTests.h
@@ -0,0 +1,13 @@
+//
+//  FBFriendListTests.h
+//  My FB friends
+//
+//  Created by Emre Ergenekon on 2/8/12.
+//  Copyright (c) 2012 Bontouch AB, Codely HB. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface FBFriendListTests : SenTestCase
+
+@end
---
+++ My FB friends/LogicUnitTests/FBFriendListTests.m
@@ -0,0 +1,30 @@
+//
+//  FBFriendListTests.m
+//  My FB friends
+//
+//  Created by Emre Ergenekon on 2/8/12.
+//  Copyright (c) 2012 Bontouch AB, Codely HB. All rights reserved.
+//
+
+#import "FBFriendListTests.h"
+
+@implementation FBFriendListTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+}
+
+- (void)tearDown
+{
+    
+    [super tearDown];
+}
+
+- (void)testFail
+{
+    STFail(@"This is meant to fail");
+}
+
+@end
